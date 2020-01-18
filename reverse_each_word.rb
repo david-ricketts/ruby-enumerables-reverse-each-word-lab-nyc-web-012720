23237b95cdf09636a0bab643f55a55896a816018
David Ricketts <david.ricketts119@gmail.com>
2020-01-18 23:19:24 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word (str)
+  words = str.split()
+  r_words = []
+  
+  
+  words.each do |word| 
+    r_words << word.reverse
+  end
+  
+  return r_words.join(" ")
+end
+
+
+def  expect_any_instance_of(array)
+  sentences = array
+  
+  
+  r_sentences =  sentences.collect do  |sentence| reverse_each_word(sentence)
+ end
+  
+  return r_sentences(" ")
+end
